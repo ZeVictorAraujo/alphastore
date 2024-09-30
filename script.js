56c8d7886031972315006c992566a0ed5adf0451
ZeVictorAraujo <josearaujo.ba@hotmail.com>
2024-09-29 22:59:05 -0300

    FInalização

--- script.js
+++ script.js
@@ -4,16 +4,27 @@ document.addEventListener("DOMContentLoaded", function() {
     const menuContato = document.getElementById('menu-contato');
     const homeSection = document.getElementById('home-section');
     const contactSection = document.getElementById('contact-section');
+    const buttoncar = document.getElementById('carrinho');
+    const buttoncarSection = this.getElementById('carrinho-section')
 
     // Mostrar Home ao clicar no menu "INÍCIO"
     menuHome.addEventListener('click', function() {
         homeSection.style.display = 'block';
         contactSection.style.display = 'none';
+        buttoncarSection.style.display = 'none';
     });
 
     // Mostrar Contato ao clicar no menu "CONTATO"
     menuContato.addEventListener('click', function() {
         homeSection.style.display = 'none';
         contactSection.style.display = 'block';
+        buttoncarSection.style.display = 'none';
     });
+
+    buttoncar.addEventListener('click', function() {
+        homeSection.style.display = 'none';
+        contactSection.style.display = 'none';
+        buttoncarSection.style.display = 'block';
+    })
+
 });
